58f159d0cc8576640c36d666886d7d0156d083c4
blackironj <blackironj@gmail.com>
2019-08-04 14:13:10 +0900

    # EDIT - app version / first run info / gitignore

--- app/src/main/java/com/just3days/config/config.kt
+++ app/src/main/java/com/just3days/config/config.kt
@@ -1,7 +1,6 @@
 package com.just3days.config
 
-const val THREE_DAYS: Int = 60 // Test time
-//const val THREE_DAYS: Int = 259200 // Time (seconds)
+const val THREE_DAYS: Int = 259200 // Time (seconds)
 const val NON_TRANSPARENCY: Int = 255
 
 object AppConstants {
@@ -10,5 +9,7 @@ object AppConstants {
             "작심 내용은 3일동안 서서히 사라집니다.\n\n" +
             "작심이 사라지는 것을 막기 위해, 하단의 心을 눌러 작심을 다시 하시길 권해드립니다.\n\n" +
             "작심 내용을 변경하기 위해서는 3일 뒤에 기존의 작심이 완전히 사라지고 난 다음 새로 입력이 가능합니다.\n\n" +
-            "한 번 작심을 할때 주의를 기울여 주세요.\n\n"
+            "한 번 작심을 할때 주의를 기울여 주세요.\n\n\n" +
+            "* 결심의 내용은 짧게 작성해주세요. (최대 20자)\n\n" +
+            "* 결심 후, 다시 心을 누르게되면 해당 시점부터 다시 3일이 진행됩니다."
 }
